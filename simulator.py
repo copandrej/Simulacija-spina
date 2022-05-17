79e0eb1a3d9518998309c4c6a025902488526741
copandrej <ac8560@student.uni-lj.si>
2022-05-17 17:14:54 +0200

    simulation in theory...

--- simulator.py
+++ simulator.py
@@ -1,84 +1,67 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 import random
 import math
 import cmath
 from qutip import *
 
+# parametri
+t = 0.05  #časovni interval za meritve
+T = math.pi  # 4*T je trenutno en obhod
+time = theta = phi = 0 #zač. vrednosti
+frekvenca = 1 #frekvenca ni pomembna saj nimamo podane velikosti mag. polje, od katere je odvisna
 
-"""
-IDEJA:
-  a) Simuliram spin v odvisnosti od časa po polju v smeri X xsim()
-  b) Simuliram spin v odvisnosti od časa po polju v smeri Z zsim()
-
-  pseudo
-  sim1(b):
-    Vzamem nek T in b
-    T časa laufam simulacijo a) xsim()
-    bT časa laufam simulacijo b) xsim()
-
-  sim2:
-    generiram različne b in simulriam sim1(b)
-  
-  main:
-    glavni program laufa sim2, plota, oz če to ne bo delal izpiše pogruntane rezultate
-
-  xsim(T):
-    za nek majhen deltaT se kliče hermitska funkcija na nekem začetnem vekrju v odvisnosti od časa izpeljana v učbeniku in rezultate shrani kot vektor
-    to se kliče T časa. oz. sej ne rabim to večkrat klicat right? samo časovno razliko od prejšnje simulacije uzamem? al? t+=T ?
-   zsim(b*T)
-    na isto sceno samo b*T časa
-
-
-"""
-#par
-t = 0.05 #short interval
-T = 5 #ms ?
-time = 0
-theta = 0
-phi = 0+0j
-frekvenca = 1+0j
-
-upSpin = basis(2, 0)
+#bazni vektorji
+upSpin = basis(2, 0) 
 downSpin = basis(2, 1)
 
+plus = (upSpin + downSpin)/math.sqrt(2) # | + >
+minus = (upSpin - downSpin)/math.sqrt(2)# | - >
 
-def xPsi(T):
-  upSpin = basis(2, 0)
-  downSpin = basis(2, 1)
-  debug = ((-1*(1j)*(phi+frekvenca*T))/2)
-  n = cmath.cos(theta/2)*cmath.e**debug
-  upSpin = upSpin * n
-  debug = ((1j*(phi+frekvenca*T))/2)
-  n = cmath.sin(theta/2)*cmath.e**debug
-  downSpin =  downSpin*n
-  vec = downSpin+upSpin
-  return vec
 
-def zSim(T):
-  print("zSim")
+def Psi(T, base1, base2, phi, theta):
+    scalar = (-1 * (1j) * (phi + frekvenca * T)) / 2
+    n = cmath.cos(theta / 2) * cmath.e ** scalar
+    base1 = base1 * n
+    scalar = ((1j * (phi + frekvenca * T)) / 2)
+    n = cmath.sin(theta / 2) * cmath.e ** scalar
+    base2 = base2 * n
+    vec = base2 + base1
+    return vec
 
+def zPsi(T, phi, theta):
+    return Psi(T, upSpin, downSpin, phi, theta)
+
+def xPsi(T, phi, theta):
+    return Psi(T, plus, minus, phi, theta)
+
+#simulacija v odvisnosti od parametra b
 def simulation(b):
-  time = 0
-  while time < T:
-    xSim(time)
-    zSim(b*time)
-    time += t
-    print("sim")
+    time = phi = theta = 0
+    for i in range(10): #time range 
+      while time <= T:
+        vec = zPsi(t, phi, theta) #tukaj plotamo
+        phi += t*frekvenca
+        time += t
+      time = 0
+      while time <= T:
+        vec = xPsi(t, phi, theta) #tukaj plotamo
+        theta += t*frekvenca
+        time += t
+      print("sim")
 
-#up.dag() creates bra
-"""ket = (basis(2, 0) + basis(2, 1)).unit()
-sigmap() * spin
-print(sigmap() * spin)
-print(sigmax()*spin)"""
+phi = 0
+theta = 0
+b = 0.5
+for i in range(4):
+  #vec = zPsi(T, phi,theta)
+  #phi+=T*frekvenca
+  vec = xPsi(T*b, phi, theta)
+  theta+=T*b*frekvenca
 
-#xPsi(2)
-print(xPsi(2))
+print(vec)
 
-"""
 for i in range(5):
   b = random.randint(0, 100)/100
   print("pri b =", b, "je simulacije taka:")
   simulation(b)
-"""
